023d70fa6316fc6f7dcd99004d292c6bff62b447
Tyler Gibson <tyler@thetylergibson.com>
2025-04-20 09:25:41 -0700

    add refactored tile class

--- firmware/lib/cw-gfx-engine/Tile.h
+++ firmware/lib/cw-gfx-engine/Tile.h
@@ -1,26 +1,55 @@
 #pragma once
 
 #include "Locator.h"
+#include "Sprite.h"
+#include <memory>
+
+enum class TileDirection {
+    RIGHT,
+    LEFT,
+    UP,
+    DOWN
+};
 
 struct Tile {
-  // const unsigned short *_image;
-  // int _width;
-  // int _height;
+    std::shared_ptr<Sprite> _sprite;
+    int _spacing;
+    TileDirection _direction;
 
-  // Tile(const unsigned short *image, int width, int height) {
-  //   this->_image = image;
-  //   this->_width = width;
-  //   this->_height = height;
-  // }
-  
-  // void draw(int x, int y) {
-  //   Locator::getDisplay()->drawRGBBitmap(x, y, _image, _width, _height);
-  // }
+    Tile(int spacing = 0, TileDirection direction = TileDirection::RIGHT) 
+        : _sprite(nullptr), _spacing(spacing), _direction(direction) {}
 
-  // void fillRow(int y) {    
-  //   for (int xx = 0; xx < DISPLAY_WIDTH; xx += _width) {
-  //     draw(xx, y);      
-  //   }    
-  // }
+    void setSprite(std::shared_ptr<Sprite> sprite) {
+        _sprite = sprite;
+    }
 
+    void draw(int x, int y, int count) {
+        if (!_sprite) return;
+
+        int spriteWidth = _sprite->getWidth();
+        int spriteHeight = _sprite->getHeight();
+
+        for (int i = 0; i < count; i++) {  // Start from 1 to skip first instance
+            int drawX = x;
+            int drawY = y;
+
+            switch (_direction) {
+                case TileDirection::RIGHT:
+                    drawX = x + i * (spriteWidth + _spacing);
+                    break;
+                case TileDirection::LEFT:
+                    drawX = x - i * (spriteWidth + _spacing);
+                    break;
+                case TileDirection::DOWN:
+                    drawY = y + i * (spriteHeight + _spacing);
+                    break;
+                case TileDirection::UP:
+                    drawY = y - i * (spriteHeight + _spacing);
+                    break;
+            }
+
+            _sprite->setPosition(drawX, drawY);
+            Locator::getCanvas()->drawRGBBitmap(drawX, drawY, _sprite->getCurrentSprite(), _sprite->getCurrentMask(), spriteWidth, spriteHeight);
+        }
+    }
 };
